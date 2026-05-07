2a8c988b1b2dc6a0dc54da042456e8f68b116b7d
amit-kumar1511 <amit55421kumar@gmail.com>
2026-05-07 12:00:50 +0530

    update frontend backend folder

---
+++ backend/create-env.js
@@ -0,0 +1,3 @@
+import fs from 'fs';
+fs.writeFileSync('.env', 'MONGO_URI=mongodb+srv://amitkumar55423_db_user:S4art9ieShXj0mA5@cluster0.eh3ygbz.mongodb.net/?appName=Cluster0');
+console.log('.env file created successfully');
